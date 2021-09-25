f43edf39a8ca8a06950edc24cf85d81ea385234a
Alec Roberson <alec@Alecs-MacBook-Pro.local>
2021-09-25 12:07:55 -0700

    made netdev scripts runnable via command line

--- netdev/__main__.py
+++ netdev/__main__.py
@@ -9,29 +9,29 @@ from netdev.arch_test import arch_test
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # subparser argument
-    subparsers = parser.add_subparsers(title='command', dest='command')
+    subparsers = parser.add_subparsers(title='function', dest='function')
     # subparser for anchor finding
-    F_parser = subparsers.add_parser('af', help='anchor box finding')
-    F_parser.add_argument('data_path', type=str,
+    af_parser = subparsers.add_parser('af', help='anchor box finding')
+    af_parser.add_argument('data_path', type=str,
         help='the path to the raw data, where labels are all stored in a .txt format')
-    F_parser.add_argument('input_dim', type=int,
+    af_parser.add_argument('input_dim', type=int,
         help='the input dimension of images to the network')
-    F_parser.add_argument('num_anchors', type=int,
+    af_parser.add_argument('num_anchors', type=int,
         help='the number of anchors that the network should have (total)')
     # subparser for architecture testing
-    T_parser = subparsers.add_parser('at', help='architecture testing')
-    T_parser.add_argument('cfg_path',
+    at_parser = subparsers.add_parser('at', help='architecture testing')
+    at_parser.add_argument('cfg_path',
         help='the relative path to the config (.cfg) file to test')
-    T_parser.add_argument('--batch_size', dest='batch_size', default=32,
+    at_parser.add_argument('--batch_size', dest='batch_size', default=32,
         type=int, help='the batch size (default: 32)')
-    T_parser.add_argument('--stop_at', dest='stop_at', default=None,
+    at_parser.add_argument('--stop_at', dest='stop_at', default=None,
         type=int, help='after which layer to stop the testing loop, if desired')
     # parse arguments
     args = parser.parse_args()
 
     # run the command
-    if args.command == 'F':
+    if args.function == 'af':
         find_anchors(args.data_path, args.input_dim, args.num_anchors)
-    elif args.command == 'T':
+    elif args.function == 'at':
         # architecture testing
         arch_test(args.cfg_path, args.batch_size, args.stop_at)
